42b6174202a8a0e02d9896dbd6dc7d5a337d2fa6
Kelly Keiko <kelly_kawase@hotmail.com>
2024-01-11 00:12:23 -0300

    Part 3: Add Features with Plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 //Step 1: Importing react
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from 'gatsby-plugin-image'
 
 //Step 2: Defining the components
 const IndexPage = () => {
   return (
    <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Frieren is the title character and protagonist of the Japanese manga series Frieren: Beyond Journey's End, written by Kanehito Yamada and illustrated by Tsukasa Abe."
+        src="../images/frieren.jpe"
+      />
    </Layout>
   )
 }
